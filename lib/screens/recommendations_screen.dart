bc972f2e9f721dea893acd4d3039b95e199e193a
vkandreli <pmkandreli@gmail.com>
2025-01-03 15:56:31 +0200

    f

--- lib/screens/recommendations_screen.dart
+++ lib/screens/recommendations_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:virgil_demo/assets/placeholders.dart';
 import 'package:virgil_demo/screens/book_presentation.dart'; // Import the MovieDetailScreen
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:location/location.dart';
 //import 'package:virgil_demo/assets/placeholders.dart';
 
 class RecommendationsScreen extends StatefulWidget {
@@ -11,42 +11,25 @@ class RecommendationsScreen extends StatefulWidget {
 }
 
 class _RecommendationsScreenState extends State<RecommendationsScreen> {
-  late GoogleMapController mapController;
-  late LocationData currentLocation;
+  // late GoogleMapController mapController;
+  // late LocationData currentLocation;
 
-  // Initialize the location package
-  Location location = Location();
+  // // Initialize the location package
+  // Location location = Location();
 
-  // Sample list of movie poster URLs and movie titles
-  // List<Map<String, String>> popularMovies = [
-  //   {"title": "Movie 1", "imageUrl": "https://via.placeholder.com/150x225?text=Movie+1"},
-  //   {"title": "Movie 2", "imageUrl": "https://via.placeholder.com/150x225?text=Movie+2"},
-  //   {"title": "Movie 3", "imageUrl": "https://via.placeholder.com/150x225?text=Movie+3"},
-  //   {"title": "Movie 4", "imageUrl": "https://via.placeholder.com/150x225?text=Movie+4"},
-  //   {"title": "Movie 5", "imageUrl": "https://via.placeholder.com/150x225?text=Movie+5"},
-  // ];
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getCurrentLocation();
+  // }
 
-  // List<Map<String, String>> newReleases = [
-  //   {"title": "New Release 1", "imageUrl": "https://via.placeholder.com/150x225?text=New+Release+1"},
-  //   {"title": "New Release 2", "imageUrl": "https://via.placeholder.com/150x225?text=New+Release+2"},
-  //   {"title": "New Release 3", "imageUrl": "https://via.placeholder.com/150x225?text=New+Release+3"},
-  //   {"title": "New Release 4", "imageUrl": "https://via.placeholder.com/150x225?text=New+Release+4"},
-  //   {"title": "New Release 5", "imageUrl": "https://via.placeholder.com/150x225?text=New+Release+5"},
-  // ];
-
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentLocation();
-  }
-
-  // Fetch current location of the user
-  Future<void> _getCurrentLocation() async {
-    LocationData locationData = await location.getLocation();
-    setState(() {
-      currentLocation = locationData;
-    });
-  }
+  // // Fetch current location of the user
+  // Future<void> _getCurrentLocation() async {
+  //   LocationData locationData = await location.getLocation();
+  //   setState(() {
+  //     currentLocation = locationData;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -54,24 +37,24 @@ class _RecommendationsScreenState extends State<RecommendationsScreen> {
       body: SafeArea(
         child: ListView(
           children: [
-            // Google Map above New Releases
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 300, // Height for the Google Map
-                child: currentLocation != null
-                    ? GoogleMap(
-                        initialCameraPosition: CameraPosition(
-                          target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
-                          zoom: 14.0,
-                        ),
-                        onMapCreated: (GoogleMapController controller) {
-                          mapController = controller;
-                        },
-                      )
-                    : Center(child: CircularProgressIndicator()),
-              ),
-            ),
+            // Google Map 
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: SizedBox(
+            //     height: 300, // Height for the Google Map
+            //     child: currentLocation != null
+            //         ? GoogleMap(
+            //             initialCameraPosition: CameraPosition(
+            //               target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+            //               zoom: 14.0,
+            //             ),
+            //             onMapCreated: (GoogleMapController controller) {
+            //               mapController = controller;
+            //             },
+            //           )
+            //         : Center(child: CircularProgressIndicator()),
+            //   ),
+            // ),
 
             // Popular Right Now Header
             Padding(
